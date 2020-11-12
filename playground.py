b97b7b9999f8d32ce73c3ec4e9d64abadef867a8
waverDeep <dandyleader@naver.com>
2020-11-12 21:50:35 +0900

    실행코드 작성

--- playground.py
+++ playground.py
@@ -2,5 +2,7 @@ import fileIO
 import useG2Pk
 
 
-filepath = ''
+filepath = 'dataset/total_data.csv'
 scripts = fileIO.read_csv_pandas(filepath=filepath)
+g2pk_scripts = useG2Pk.convert_g2pk_scripts_pandas(scripts)
+print(len(g2pk_scripts))
